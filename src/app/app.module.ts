7a7c384a74c227fe9e3114c391b96dda2cdd7d5a
Srdjan Hajder <srdjan.hajder@bam.fan>
2022-05-09 20:35:40 +0200

    Add hero crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
 	declarations: [
@@ -15,9 +19,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 		HeroesComponent,
 		HeroDetailComponent,
 		MessagesComponent,
-  DashboardComponent,
+		DashboardComponent,
 	],
-	imports: [BrowserModule, FormsModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		AppRoutingModule,
+		HttpClientModule,
+		HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+			dataEncapsulation: false,
+		}),
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
